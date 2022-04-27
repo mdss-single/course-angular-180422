31779387ad611bc7c42a96362843b4a1b4302851
Alexey Bulygin <alexey.bulygin@webasyst.ru>
2022-04-27 11:18:34 +0300

    feat: add header & sidenav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SidenavModule } from './components/sidenav/sidenav.module';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, HeaderComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		SidenavModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
